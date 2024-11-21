39643f6bb22c66b880a25b2a01f6c3d89a288ab0
AdrianJhudiel <jhudzartezuela@gmail.com>
2024-11-21 12:42:30 +0800

    asdas
    dasdasd

--- myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, ParkingArea, Sticker, Vehicle, ParkingLot
-
-admin.site.register(User)
-admin.site.register(ParkingArea)
-admin.site.register(Sticker)
-admin.site.register(Vehicle)
-admin.site.register(ParkingLot)
